68b8814ca32561f47b281915b5e691df0adf7606
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-01-24 19:41:58 -0500

    finalizing finished project for last assignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,67 @@
+
 // TODO: Declare any global variables we need
+let headsRolls = 0 
+let tailsRolls = 0
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+document.addEventListener('DOMContentLoaded', function() {
 
     // TODO: Add event listener and handler for flip and clear buttons
-
+    document.getElementById('flip').addEventListener('click', () => {
+    let flippedHeads = Math.random() < 0.5
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
+    if (flippedHeads) {
+        document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+        document.getElementById('message').textContent = 'You flipped heads!'
 
+        headsRolls += 1
+    }
+    else {
+        document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+        document.getElementById('message').textContent = 'You flipped tails!'
+
+        tailsRolls += 1
+    }
+    
+    let total = headsRolls + tailsRolls
+
+   
+
+    if (total > 0){
+
+        let percentHeads = Math.round(headsRolls / total) * 100
+        let percentTails = Math.round(tailsRolls / total) * 100
+    }
+
+    document.getElementById('heads').textContent = headsRolls
+    document.getElementById('heads-percent').textContent = percentHeads + '%'
+    document.getElementById('tails').textContent = tailsRolls
+    document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
+
+    document.getElementById('clear').addEventListener('click', function() {
+        headsRolls = 0
+        tailsRolls = 0
+
+        document.getElementById('message').textContent = 'Let\'s get rolling!'
+
+        let total = headsRolls + tailsRolls
+
+        let percentHeads = 0
+        let percentTails = 0
+
+        if (total > 0) {
+
+            percentHeads = math.round((headsRolls / total) * 100)
+            percentTails = math.round((tailsRolls / total) * 100)
+        }
+
+        document.getElementById('heads').textContent = headsRolls
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails').textContent = tailsRolls
+        document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
